45b4fd274742366c6592e5d8f1e3866f7759cbee
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-05-26 19:33:08 +0200

    Migrate DB
    After generating the scaffold, its necessary to migrate the database to the latest state with this command:
    'rake db:migrate'
    Also fixed image names

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180522190338) do
+ActiveRecord::Schema.define(version: 20180526171123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20180522190338) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "tool_consumers", force: :cascade do |t|
+    t.string "name"
+    t.string "key"
+    t.string "secret"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
