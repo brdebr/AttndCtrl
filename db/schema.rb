c2498fc1ed11fe40d0d6c31e2f6c3db2da4bb532
Bryan de Oliveira Brettas <brdebr.93@gmail.com>
2018-06-07 21:53:08 +0200

    Add lti_id param to LtiUser
    This change adds a migration with this command:
    $- rails g migration AddLtiIdToLtiUser lti_id:integer
    Also migrates the database with the change

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180607193514) do
+ActiveRecord::Schema.define(version: 20180607195057) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20180607193514) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "lti_id"
   end
 
   create_table "timetable_units", force: :cascade do |t|
